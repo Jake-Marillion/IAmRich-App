db90c866b738e4fa45a3fc2344bb70b712241528
Jacob Marillion <110437842+Jake-Marillion@users.noreply.github.com>
2022-12-13 18:50:40 -0700

    Add files via upload

---
+++ IAmRich App/IAmRich App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmRich App
+//
+//  Created by Jacob Marillion on 12/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   @IBOutlet weak var dimond: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
